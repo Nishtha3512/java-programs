89d883f410bbe14e2dd18484d96430fc887938fa
Nishtha3512 <51858166+Nishtha3512@users.noreply.github.com>
2019-08-04 15:53:33 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+class Arithmetic
+{
+public static void main(String args[])
+{
+int a,b,c,d,e;
+a=10;
+b=5;
+c=a+b;
+d=a%b;
+e=a/b;
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+}
+}
---
+++ Logical.java
@@ -0,0 +1,13 @@
+public class Logical
+{
+public static void main(String args[])
+{
+int x=10;
+int y=5;
+int z=4;
+System.out.println(x>y && y>z);
+System.out.println(x>y || y>z);
+System.out.println(!(x>y));
+System.out.println(~x);
+}
+}
